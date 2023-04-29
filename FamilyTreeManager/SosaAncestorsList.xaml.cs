bce26d50b4a1092e8bca1d2b28328104c350d5f6
Marek <mar94bor@gmail.com>
2023-04-29 02:09:17 +0200

    Distinguishing people in Sosa List made.

--- FamilyTreeManager/SosaAncestorsList.xaml.cs
+++ FamilyTreeManager/SosaAncestorsList.xaml.cs
@@ -23,23 +23,23 @@ namespace FamilyTreeManager
         internal SosaAncestorsList(Person probant)
         {
             InitializeComponent();
-            List<(int number, Person person)> ancestors = GetAncestorsInBFSOrder(probant);
+            List<(int sosaNumber, Person person)> ancestors = GetAncestorsInBFSOrder(probant);
             ShowAncestors(ancestors);
         }
 
-        private List<(int number, Person person)> GetAncestorsInBFSOrder(Person probant)
+        private List<(int sosaNumber, Person person)> GetAncestorsInBFSOrder(Person probant)
         {
-            Queue<(int number, Person person)> queue = new Queue<(int, Person)>();
-            List<(int number, Person person)> ancestors = new List<(int, Person)>();
+            Queue<(int sosaNumber, Person person)> queue = new Queue<(int, Person)>();
+            List<(int sosaNumber, Person person)> ancestors = new List<(int, Person)>();
 
             queue.Enqueue(new(1, probant));
             ancestors.Add(new(1, probant));
 
             while (queue.Count != 0)
             {
-                (int number, Person person) tuple = queue.Dequeue();
+                (int sosaNumber, Person person) tuple = queue.Dequeue();
                 Person currentPerson = tuple.person;
-                int sosaNumber = tuple.number;
+                int sosaNumber = tuple.sosaNumber;
 
                 foreach (Person parent in currentPerson.GetParents)
                     if (parent != null)
@@ -56,15 +56,15 @@ namespace FamilyTreeManager
             return ancestors;
         }        
 
-        private void ShowAncestors(List<(int number, Person person)> ancestors)
+        private void ShowAncestors(List<(int sosaNumber, Person person)> ancestors)
         {
             int currentGenerationNumber = 0;
 
-            foreach ((int number, Person person) tuple in ancestors)
+            foreach ((int sosaNumber, Person person) tuple in ancestors)
             {
-                double parentGeneration = Math.Floor(Math.Log2(tuple.number)) + 1;
+                double parentGenerationNumber = Math.Floor(Math.Log2(tuple.sosaNumber)) + 1;
 
-                if (parentGeneration > currentGenerationNumber)
+                if (parentGenerationNumber > currentGenerationNumber)
                 {
                     currentGenerationNumber++;
 
@@ -72,7 +72,7 @@ namespace FamilyTreeManager
                     {
                         Text = $"Pokolenie {currentGenerationNumber}",
                         FontWeight = FontWeights.Bold,
-                        FontSize = 16,
+                        FontSize = 16
                     };
 
                     stackPanel.Children.Add(new Label());
@@ -81,28 +81,86 @@ namespace FamilyTreeManager
 
                 TextBlock personTextBlock = new TextBlock()
                 {
-                    Text = $"{tuple.number}. {tuple.person.ShowLongerPersonDescription()}",
+                    Text = $"{tuple.sosaNumber}. {tuple.person.ShowLongerPersonDescription()}",
                     FontSize = 16,
-                    DataContext = tuple.person
+                    DataContext = tuple
                 };
 
+                personTextBlock.MouseRightButtonDown += textBlock_RightClick;
                 stackPanel.Children.Add(personTextBlock);
             }
         }
 
-        private void lolo_Click(object sender, RoutedEventArgs e)
+        private (int sosaNumber, Person person) _clickedPersonWithSosaNumber;
+
+        private void textBlock_RightClick(object sender, RoutedEventArgs e)
+        {           
+            ContextMenu contextMenu = FindResource("cmTextBlock") as ContextMenu;
+            contextMenu.PlacementTarget = sender as TextBlock;
+            contextMenu.IsOpen = true;
+
+            _clickedPersonWithSosaNumber = ((int sosaNumber, Person person))(sender as TextBlock).DataContext;
+        }
+
+        private int _counter = 0;
+
+        private void MenuItem1_Click(object sender, RoutedEventArgs e)
         {
-            foreach (var item in stackPanel.Children)
+            DistinguishPeopleIndicatedByCondition(tuple => tuple.person == _clickedPersonWithSosaNumber.person);
+            infoTextBlock.Text = $"{_clickedPersonWithSosaNumber.person} występuje {_counter} razy";
+            _counter = 0;
+        }
+
+        private void MenuItem2_Click(object sender, RoutedEventArgs e)
+        {
+            int sosaNumber = _clickedPersonWithSosaNumber.sosaNumber;
+            List<int> sosaNumbers = new List<int> { sosaNumber };
+
+            while (sosaNumber != 1)
             {
-                var t = item as TextBlock;
-
-                if(t is TextBlock)
-                {
-                    if (t.Text.Contains("some text"))
-                        t.FontWeight = FontWeights.Bold;
-                }
-
+                sosaNumber /= 2;
+                sosaNumbers.Add(sosaNumber);
             }
+
+            DistinguishPeopleIndicatedByCondition(tuple => sosaNumbers.Contains(tuple.sosaNumber));
+            infoTextBlock.Text = "";
+            _counter = 0;
+        }
+
+        private void DistinguishPeopleIndicatedByCondition(Func<(int sosaNumber, Person person), bool> predicate)
+        {
+            foreach (var stackPanelElement in stackPanel.Children)
+            {
+                TextBlock textBlock = stackPanelElement as TextBlock;
+
+                if (textBlock is TextBlock && textBlock.DataContext != null)
+                {
+                    (int sosaNumber, Person person) currentPersonWithSosaNumber = ((int sosaNumber, Person person))textBlock.DataContext;
+                    
+                    textBlock.Text = textBlock.Text.Trim();
+
+                    if (predicate(currentPersonWithSosaNumber))
+                    {
+                        textBlock.Text = $"  {textBlock.Text}";
+                        textBlock.FontWeight = FontWeights.Bold;
+                        textBlock.FontStyle = FontStyles.Italic;
+                        textBlock.Foreground = Brushes.DarkRed;
+
+                        kfddl();
+                    }
+                    else
+                    {
+                        textBlock.FontWeight = FontWeights.Normal;
+                        textBlock.FontStyle = FontStyles.Normal;
+                        textBlock.Foreground = Brushes.Black;
+                    }
+                }
+            }
+        }
+
+        private void kfddl()
+        {
+            _counter++;
         }
     }
 }
